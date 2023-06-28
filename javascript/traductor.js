d5f412e86ba2fc0f02eb18efe3ffe03afe21012c
Sebastian Castillo <Sebastian Castillo>
2023-06-27 19:49:45 -0500

    add styles header and footer

--- javascript/traductor.js
+++ javascript/traductor.js
@@ -1,17 +1,10 @@
 let check = document.querySelector(".check");
-check.addEventListener('click', () => {
-
+check.addEventListener("click", () => {
     let id = check.checked;
 
     if (id == true) {
-
         location.href = "../languages/index.html";
-
     } else {
-
         location.href = "../index.html";
-
-
     }
-
 });
